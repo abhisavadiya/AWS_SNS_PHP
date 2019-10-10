466ecdd5bd488c963be80b42dd504c5bb5436962
abhisavadiya <abhisavadiya@gmail.com>
2019-10-11 00:25:48 +0530

    Update sms.php

--- sms.php
+++ sms.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 	$sdk = new Aws\Sns\SnsClient([
 		'region'  => 'ap-southeast-1',
 		'version' => 'latest',
-		'credentials' => ['key' => 'Your_Key', 'secret' => 'Your_Secret']
+		'credentials' => ['key' => 'AKIA6FGFUGLZUHFCHHDR', 'secret' => 'wRda4qvrfa4PnMRWM//ELu9fOfEOtaHVUQUG4jro']
 	]);
 	$result1 = $sdk->SetSMSAttributes([
         'attributes' => [
@@ -13,7 +13,7 @@ require 'vendor/autoload.php';
     ]);
 $result = $sdk->publish([
     'Message' => 'Your Message',
-    'PhoneNumber' => 'Your Phonenumber',
+    'PhoneNumber' => '+919824618237',
     'MessageAttributes' => ['AWS.SNS.SMS.SenderID' => [
          'DataType' => 'String',
          'StringValue' => 'WebNiraj'
